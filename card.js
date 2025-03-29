c1e6bc818ead5c126edb8b7053307029cef8763d
Danamelisa-g <danamelisag17@gmail.com>
2025-03-28 19:00:55 -0500

    commit 3

--- card.js
+++ card.js
@@ -1,10 +1,17 @@
 class Card extends HTMLElement {
     constructor () {
         super()
-        
         this.shadowRoot
+        this.container = document.createElement("container")
+
     }
 
-    
+    connectedCallback() {
+        console.log("Card");
+    }
+
+   
+
+
 
 }
